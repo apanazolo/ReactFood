e208f8972bba5665ba9c6b9da6a862bb828fe3c1
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-08-31 16:29:09 -0300

    REMOVED NECESSARY LOGIC IN APP.JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import { useState } from 'react';
-
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-import Cart from './components/Cart/Cart';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
     <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
+      <Header />
       <main>
         <Meals />
       </main>
