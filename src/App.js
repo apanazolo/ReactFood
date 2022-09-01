37e620e879230223bbe4a352979208ad4d21818f
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2022-09-01 01:06:22 -0300

    APP.JS ALTERED BY MAX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <CartProvider>
       {cartIsShown && <div>Cart...</div>}
